5c3565fb397c99b816fe3e73e21174cd681f2b75
blendy9702 <blendy9702@gmail.com>
2024-12-20 15:14:17 +0900

    [feat]회원가입 가능 추가

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-  const [formData, setFormData] = useState();
-  const [isLogin, setIsLogin] = useState();
-
   return (
     <div>
       <div className="loginTopArea">
