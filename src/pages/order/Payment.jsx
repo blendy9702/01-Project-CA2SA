dc396aa9bf24727301c20229b7a448d8adbf2a6a
200Bingsu <200bingsu@gmail.com>
2024-12-22 21:19:21 +0900

    [feat] menuDetail 금액 작업 예정

--- src/pages/order/Payment.jsx
+++ src/pages/order/Payment.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { OrderContext } from "../../contexts/OrderContext";
 import { useLocation, useSearchParams } from "react-router-dom";
 
@@ -6,7 +6,7 @@ const Payment = () => {
   const { order } = useContext(OrderContext);
   // order가 제대로 바뀌고 있는지 확인
   useEffect(() => {
-    console.log("현재 order 상태:", order);
+    // console.log("현재 order 상태:", order);
   }, [order]);
 
   const location = useLocation();
@@ -15,7 +15,27 @@ const Payment = () => {
   // 쿼리 스트링 주소 처리
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const sumOrder = order.map((item, index) => {});
+  //중복 메뉴 합치기
+  //임시 메뉴 리스트 부여
+  const orderList = [...order.menu];
+  console.log("장바구니 목록:", orderList);
+  const stackedOrder = orderList.reduce((acc, curr) => {
+    const existingOrder = acc.find(
+      item =>
+        item.menuId === curr.menuId &&
+        item.state === curr.state &&
+        item.size === curr.size &&
+        JSON.stringify(item.beans) === JSON.stringify(curr.beans) &&
+        JSON.stringify(item.addOption) === JSON.stringify(curr.addOption),
+    );
+    if (existingOrder) {
+      existingOrder.count += curr.count;
+    } else {
+      acc.push({ ...curr });
+    }
+    return acc;
+  }, []);
+  console.log("정리된 배열:", stackedOrder);
 
   return (
     <div>
