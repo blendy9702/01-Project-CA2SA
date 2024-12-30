fcad082368acb34c0dc414bbdf0564c1e7d4e008
blendy9702 <blendy9702@gmail.com>
2024-12-30 15:52:37 +0900

    [feat] 회원가입 CSS중

--- src/styles/join/joinpage.js
+++ src/styles/join/joinpage.js
@@ -1,10 +1,40 @@
 import styled from "@emotion/styled";
 
 export const JoinPageWrap = styled.div``;
-export const joinPageTopArea = styled.div``;
-export const joinPageTopBackOff = styled.div``;
-export const joinPageTopText = styled.div`
+export const JoinPageTopArea = styled.div`
+  display: flex;
+`;
+export const JoinPageTopBackOff = styled.div``;
+export const JoinPageTopText = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  font-size: 20px;
+  padding-left: 260px;
+  margin-top: 10px;
 `;
+
+export const JoinPageMainWrap = styled.div``;
+
+export const JoinPageTextArea = styled.div`
+  p {
+    font-size: 12px;
+    margin-top: 10px;
+    margin-bottom: 2px;
+  }
+  span {
+    font-size: 20px;
+    font-weight: 700;
+    display: flex;
+    padding-top: 50px;
+  }
+`;
+
+export const JoinPageNickName = styled.div``;
+export const JoinPageEmail = styled.div``;
+export const JoinPagePassword = styled.div``;
+export const JoinPageCheckArea = styled.div``;
+export const ServiceCheckBox = styled.div``;
+export const EssentialRadioBox = styled.div``;
+export const ChoiceRadioBox = styled.div``;
+export const JoinPageMoveNext = styled.div``;
